5ab3f7dd94319bfdce3b78d4c5f8b6d8eb29a471
Dmitriy E <father.06@bk.ru>
2023-09-10 22:22:35 +0300

    homework

---
+++ main.py
@@ -0,0 +1,99 @@
+class Student:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+        self.average_rating = float()
+
+    def __str__(self):
+        grades_count = 0
+        courses_in_progress_string = ', '.join(self.courses_in_progress)
+        finished_courses_string = ', '.join(self.finished_courses)
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+        res = f'Имя: {self.name}\n' \
+              f'Фамилия: {self.surname}\n' \
+              f'Средняя оценка за домашнее задание: {self.average_rating}\n' \
+              f'Курсы в процессе обучения: {courses_in_progress_string}\n' \
+              f'Завершенные курсы: {finished_courses_string}'
+        return res
+
+    def rate_hw(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_in_progress and course in lecturer.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.average_rating = float()
+        self.grades = {}
+
+    def __str__(self):
+        grades_count = 0
+        for k in self.grades:
+            grades_count += len(self.grades[k])
+        self.average_rating = sum(map(sum, self.grades.values())) / grades_count
+        res = f'Имя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rating}'
+        return res
+
+
+class Reviewer(Mentor):
+    def rate_hw(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
+    def __str__(self):
+        some_reviewer = f'Имя: {self.name}\nФамилия: {self.surname}'
+
+        return some_reviewer
+
+
+cool_reviewer_1 = Reviewer('Some', 'Buddy')
+cool_reviewer_1.courses_attached += ['Python']
+cool_reviewer_1.courses_attached += ['C++']
+
+best_lecturer_1 = Lecturer("Ivan", "Ivanov")
+best_lecturer_1.courses_attached += ['Python']
+
+student_1 = Student('Denis', 'Denisov')
+student_1.courses_in_progress += ['Python']
+student_1.finished_courses += ['Введение в программирование']
+
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+student_1.rate_hw(best_lecturer_1, 'Python', 10)
+
+cool_reviewer_1.rate_hw(student_1, 'Python', 8)
+cool_reviewer_1.rate_hw(student_1, 'Python', 9)
+cool_reviewer_1.rate_hw(student_1, 'Python', 10)
+
+print(f'Перечень студентов:\n\n{student_1}\n\n')
+print()
+
+print(f'Перечень лекторов:\n\n{best_lecturer_1}\n\n')
+print()
+
+print(f'перечень проверяющих \n\n{cool_reviewer_1}')
+print()
